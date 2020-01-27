7de079502d537c3f08b9ae688d5fd652134eccf9
TT <ttdyce@gmail.com>
2020-01-27 17:29:13 +0800

    Add auto versioning, using git tag

--- app/src/main/java/com/github/ttdyce/nhviewer/view/SettingsFragment.java
+++ app/src/main/java/com/github/ttdyce/nhviewer/view/SettingsFragment.java
@@ -1,11 +1,12 @@
 package com.github.ttdyce.nhviewer.view;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.github.ttdyce.nhviewer.BuildConfig;
 import com.github.ttdyce.nhviewer.R;
-
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -13,6 +14,11 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        int versionCode = BuildConfig.VERSION_CODE;
+        String versionName = BuildConfig.VERSION_NAME;
+        Log.i("SettingsFragment", "onCreate: version name=" + versionName);
+        Log.i("SettingsFragment", "onCreate: version code=" + versionCode);
     }
 
     @Override
