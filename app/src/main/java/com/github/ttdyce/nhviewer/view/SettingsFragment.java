ab22765a6480e5bc3744f259a38db11c4e208b59
TT <ttdyce@gmail.com>
2021-01-20 21:21:22 +0800

    Migrate in-app update from firebase remote config to app-center distribution

--- app/src/main/java/com/github/ttdyce/nhviewer/view/SettingsFragment.java
+++ app/src/main/java/com/github/ttdyce/nhviewer/view/SettingsFragment.java
@@ -1,20 +1,16 @@
 package com.github.ttdyce.nhviewer.view;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
 
 import com.github.ttdyce.nhviewer.BuildConfig;
 import com.github.ttdyce.nhviewer.R;
-import com.github.ttdyce.nhviewer.model.firebase.Updater;
+import com.microsoft.appcenter.distribute.Distribute;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
@@ -41,32 +37,34 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 Toast.makeText(requireContext(), "Checking latest version...", Toast.LENGTH_SHORT).show();
+                Distribute.checkForUpdate();
 
-                Updater.with(requireContext()).onUpdateNeeded(new Updater.OnUpdateNeededListener() {
-                    @Override
-                    public void onUpdateNeeded(final String updateUrl) {
-
-                        AlertDialog dialog = new AlertDialog.Builder(requireContext(), R.style.DialogTheme)
-                                .setTitle(getString(R.string.new_version_available))
-                                .setMessage(getString(R.string.new_version_desc))
-                                .setPositiveButton(getString(R.string.new_version_download_github),
-                                        new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(updateUrl));
-                                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                                startActivity(intent);
-                                            }
-                                        }).setNegativeButton(getString(R.string.new_version_cancel),
-                                        new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                dialog.dismiss();
-                                            }
-                                        }).create();
-                        dialog.show();
-                    }
-                }).check();
+                // todo: keep for 1 version. Migrate from firebase to vs-app-center
+//                Updater.with(requireContext()).onUpdateNeeded(new Updater.OnUpdateNeededListener() {
+//                    @Override
+//                    public void onUpdateNeeded(final String updateUrl) {
+//
+//                        AlertDialog dialog = new AlertDialog.Builder(requireContext(), R.style.DialogTheme)
+//                                .setTitle(getString(R.string.new_version_available))
+//                                .setMessage(getString(R.string.new_version_desc))
+//                                .setPositiveButton(getString(R.string.new_version_download_github),
+//                                        new DialogInterface.OnClickListener() {
+//                                            @Override
+//                                            public void onClick(DialogInterface dialog, int which) {
+//                                                final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(updateUrl));
+//                                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                                                startActivity(intent);
+//                                            }
+//                                        }).setNegativeButton(getString(R.string.new_version_cancel),
+//                                        new DialogInterface.OnClickListener() {
+//                                            @Override
+//                                            public void onClick(DialogInterface dialog, int which) {
+//                                                dialog.dismiss();
+//                                            }
+//                                        }).create();
+//                        dialog.show();
+//                    }
+//                }).check();
 
                 return true;
             }
